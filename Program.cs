758cd72bf83e12844cdc4aa4526e275a5b08c0a7
Mohammad Zarif <m.zarif1996@gmail.com>
2024-10-12 14:42:48 +0330

    use ef in project

--- Program.cs
+++ Program.cs
@@ -1,7 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using Xcelerate.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+// Determine which connection string to use based on the environment
+string connectionString = builder.Environment.IsDevelopment()
+    ? builder.Configuration.GetConnectionString("SQLiteConnection")! // Use SQLite for development
+    : builder.Configuration.GetConnectionString("MSSQLConnection")!; // Use MSSQL for production
+
+builder.Services.AddDbContext<XDbContext>(options =>
+{
+    if (builder.Environment.IsDevelopment())
+    {
+        options.UseSqlite(connectionString); // Use SQLite in development
+    }
+    else
+    {
+        options.UseSqlServer(connectionString); // Use SQL Server in production
+    }
+});
+
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
