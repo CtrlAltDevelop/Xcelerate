f54b34518649deb8769b8036939bbbe454bc150a
Mohammad Zarif <m.zarif1996@gmail.com>
2024-10-12 14:42:03 +0330

    add db context and data class

---
+++ Data/XDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Xcelerate.Models;
+namespace Xcelerate.Data;
+
+
+public class XDbContext : DbContext
+{
+    public XDbContext(DbContextOptions<XDbContext> options) : base(options) { }
+
+    // DbSet
+    public DbSet<ExcelData> YourEntities { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ Models/ExcelData.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Xcelerate.Models;
+
+public class ExcelData
+{
+    [Key]
+    public int Id { get; set; }  // Primary key with auto-increment
+
+    [Required]
+    public required string Field1 { get; set; }
+    [Required]
+    public required string Field2 { get; set; }
+    [Required]
+    public required string Field3 { get; set; }
+    [Required]
+    public required string Field4 { get; set; }
+    [Required]
+    public required string Field5 { get; set; }
+    [Required]
+    public required string Field6 { get; set; }
+    [Required]
+    public required string Field7 { get; set; }
+    [Required]
+    public required string Field8 { get; set; }
+    [Required]
+    public required string Field9 { get; set; }
+    [Required]
+    public required string Field10 { get; set; }
+}
